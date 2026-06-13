667149bcb1553e857dfc6bc08942a57d74cbd1e0
Himanshu Trived <trivedih748@gmail.com>
2026-06-13 05:36:38 +0530

    Fix ALLOWED_HOSTS for Render

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -38,7 +38,12 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_bool("DJANGO_DEBUG", True)
 
-ALLOWED_HOSTS = env_list("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost")
+
+    ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.onrender.com'
+]
 
 
 # Application definition
